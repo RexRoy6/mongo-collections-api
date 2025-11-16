b831810e7927bc37714bf7410ca54b90d2f3031f
Rodrigo Aguilera <rodris9806@gmail.com>
2025-11-16 12:43:38 -0700

    feat[middleware]: create basic iddleware

---
+++ app/Http/Middleware/ApiSolicitudes.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiSolicitudes
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKeyF = $request->header('API-KEY');
+        $envApiKey = env('API_KEY');
+        if ($apiKeyF == $envApiKey) {
+            return $next($request);
+        }
+
+        return response($content = 'API key inválida',$status =  401);
+    }
+}
