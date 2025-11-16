8eae7afc0dbeac493ce81d01ad67bf62eba1bc14
Rodrigo Aguilera <rodris9806@gmail.com>
2025-11-16 12:35:38 -0700

    feat[api]: add main routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\createSolicitud;
+use  App\Http\Controllers\deleteSolicitud;
+use  App\Http\Controllers\readSolicitud;
+use App\Http\Controllers\updateSolicitud;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('api.solicitudes')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//ruta de cracion:
+Route::post('/ticket', [createSolicitud::class, 'store']);
+
+
+
+//rutas metodo delete
+//esta ruta es concuidado , destuye un registro por su uuid, no usar mas que para limpiar
+Route::delete('/ticket/destroy', [deleteSolicitud::class, 'destroy']);
+//ruta para desactivar un registro de manera permanente
+Route::delete('/ticket', [deleteSolicitud::class, 'cancelSolicitud']);
+
+
+//ruta para el metodo get:
+Route::get('/ticket', [readSolicitud::class, 'read']);
+
+//ruta para modificar el status history:
+Route::put('/ticket', [updateSolicitud::class, 'update']);
 });
