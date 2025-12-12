a400cb13c32d1d19e04889f9c4f632ea8ca59b8e
Rodrigo Aguilera <rodris9806@gmail.com>
2025-12-12 11:54:42 -0700

    feat[menu]: add correct validation to menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Menu;
+use Illuminate\Support\Facades\Log;
+use App\Helpers\BusinessHelper;
 
 class MenuController extends Controller
 {
@@ -13,16 +15,28 @@ class MenuController extends Controller
      */
     public function getMenuByKey(Request $request)
     {
+    try{ 
         $validated = $request->validate([
         'menu_key' => 'required|string'
     ]);
-        $menu = Menu::where('menu_key',$validated['menu_key'])->first();
+        $menu = Menu::forCurrentBusiness()
+        ->where('menu_key',$validated['menu_key'])->first();
 
         if (!$menu) {
             return response()->json(['message' => 'Menu not found'], 404);
         }
 
         return response()->json($menu, 200);
+    
+
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        Log::error($e->getMessage());
+        return response()->json([
+            'error' => 'Bad Request',
+            'message' => 'Missing or invalid parameters',
+            'errors' => $e->errors()
+        ], 400);
     }
+}
 
 }
